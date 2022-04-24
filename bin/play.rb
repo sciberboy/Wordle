34b7c99ef84443e7b0852a67dc224ec080991665
Stephan <sciberboy@gmail.com>
2022-04-24 16:35:02 +0200

    rename play.rb

--- bin/play.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-require_relative './../config/config'
-require_relative './../lib/wordle'
-
-if $PROGRAM_NAME == __FILE__
-  word = File.readlines(Constants::WORDS).sample.chomp
-  Wordle.new(word).play
-end
