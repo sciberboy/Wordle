e6ece246129bcc660afdfce26a5e4535b303baeb
Stephan <sciberboy@gmail.com>
2022-04-26 19:06:58 +0200

    Remove customise exceptions

--- lib/exceptions.rb
+++
@@ -1,9 +0,0 @@
-module Exceptions
-  # not used - purely for future custom exceptions if needed
-
-  class Wordlength < ArgumentError
-    def initialize(message = 'Words should be be exactly 5 characters')
-      super
-    end
-  end
-end
