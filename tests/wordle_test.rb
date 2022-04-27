8fbbc71bd24e756412f6d1268e21a9337c564fb5
Stephan <sciberboy@gmail.com>
2022-04-27 12:34:12 +0200

    assert:  test if entered word in word list

--- tests/wordle_test.rb
+++ tests/wordle_test.rb
@@ -8,28 +8,21 @@ class Wordle_test < Minitest::Test
     @stubbed_input[count]
   end
 
-  def test_sword_first_guess
-    game = Wordle.new('words')
-    game.evaluate_guess($stdin.gets(1))
-    assert_equal %w[Y Y Y Y Y], game.score
-  end
-
-  def test_wordw_second_guess
+  def test_stdin_gets_wordw
     game = Wordle.new('words')
     game.evaluate_guess($stdin.gets(2))
     assert_equal %w[G G G G Y], game.score
-  end
-
-  def test_words_third_guess
-    game = Wordle.new('words')
-    game.evaluate_guess($stdin.gets(3))
-    assert_equal %w[G G G G G], game.score
   end
 
   def test_without_stdin
     game = Wordle.new('words')
     game.evaluate_guess('words')
     assert_equal %w[G G G G G], game.score
+  end
+
+  def test_word_is_not_in_wordlist
+    game = Wordle.new('swords')
+    assert_equal false, game.valid_word?('xxxxx')
   end
 
 end
