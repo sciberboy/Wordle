da9e0d1e225f93fab299487013c3ee8de47574cf
Stephan <sciberboy@gmail.com>
2022-04-27 14:37:33 +0200

    Handle words not in the word list

--- lib/constants.rb
+++ lib/constants.rb
@@ -6,5 +6,6 @@ module Constants
   YELLOW = 'Y'
   ATTEMPT_LIMIT = 6
   WORD_LENGTH = 'Please try again! The word should be exactly 5 characters in length.'
+  WORD_INVALID = 'Please try again! The word you entered is not in the word list.'
   BYE = 'Thank you for playing!'
 end
