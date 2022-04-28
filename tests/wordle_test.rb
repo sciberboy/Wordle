a0646fa99ea1b8fd1fa691a811128f3010942c88
Stephan <sciberboy@gmail.com>
2022-04-28 19:34:07 +0200

    Assert added - check for word not in list

--- tests/wordle_test.rb
+++ tests/wordle_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require_relative './../lib/wordle'
 
 class Wordle_test < Minitest::Test
+
   def $stdin.gets(count)
     @stubbed_input = %w[spacer sword wordw words]
     @stubbed_input[count]
@@ -12,6 +13,7 @@ class Wordle_test < Minitest::Test
     game = Wordle.new('words')
     game.evaluate_guess($stdin.gets(2))
     assert_equal %w[G G G G Y], game.score
+    puts game.score
   end
 
   def test_without_stdin
@@ -21,8 +23,13 @@ class Wordle_test < Minitest::Test
   end
 
   def test_word_is_not_in_wordlist
-    game = Wordle.new('swords')
+    game = Wordle.new
     assert_equal false, game.valid_word?('xxxxx')
+  end
+
+  def test_word_is_in_wordlist
+    game = Wordle.new
+    assert_equal true, game.valid_word?('sword')
   end
 
 end
