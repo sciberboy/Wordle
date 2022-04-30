d5dd7f5f200b1e9fa392ad1e96c813a1a39351bc
Stephan <sciberboy@gmail.com>
2022-04-30 20:36:14 +0200

    WIP

--- tests/wordle_test.rb
+++ tests/wordle_test.rb
@@ -10,7 +10,7 @@ class Wordle_test < Minitest::Test
   end
 
   def test_stdin_gets_wordw
-    #skip
+    skip
     game = Wordle.new('words')
     game.evaluate_guess($stdin.gets(2))
     assert_equal %w[G G G G Y], game.score
@@ -18,20 +18,32 @@ class Wordle_test < Minitest::Test
   end
 
   def test_without_stdin
-    #skip
+    skip
     game = Wordle.new('words')
     game.evaluate_guess('words')
     assert_equal %w[G G G G G], game.score
   end
 
+  def test_sword
+    game = Wordle.new('words')
+    game.evaluate_guess('sword')
+    assert_equal %w[Y Y Y Y Y], game.score
+  end
+
+  def test_match
+    game = Wordle.new('match')
+    game.evaluate_guess('watch')
+    assert_equal %w[B G G G G], game.score
+  end
+
   def test_word_is_not_in_wordlist
-    #skip
+    skip
     game = Wordle.new
     assert_equal false, game.valid_word?('xxxxx')
   end
 
   def test_word_is_in_wordlist
-    #skip
+    skip
     game = Wordle.new
     assert_equal true, game.valid_word?('sword')
   end
