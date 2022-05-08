e222e686de96a649a86e6aea328f45fefa759b5f
Stephan <sciberboy@gmail.com>
2022-05-08 21:38:55 +0200

    sanity check - update method names

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -2,13 +2,13 @@ require_relative './../config/config'
 
 class Wordle
 
-  attr_accessor  :score
+  attr_accessor  :results
 
   def initialize(word_of_the_day = '')
     @characters_of_the_day = word_of_the_day.downcase.chars
     @attempt = 1
-    @score = []
-    @history = []
+    @results = []
+    @archive = []
   end
 
   def self.play
@@ -18,17 +18,14 @@ class Wordle
   end
 
   def wordle
-    until score == Constants::ALL_GREEN || attempt > Constants::ATTEMPT_LIMIT
-      banner
-      word = evaluate(guess)
-      word.each {|character| print character.yellow << ' '}
-      puts
-      add_to_history
-      increment
-      show_score
-      puts
+    until results == Constants::ALL_GREEN || attempt > Constants::ATTEMPT_LIMIT
+      start_banner
+      user_input
+      show_results
+      archive_results
+      increment_attempt
     end
-    end_game_banner
+    end_banner
   end
 
   def valid_word?(word)
@@ -39,28 +36,34 @@ class Wordle
     false
   end
 
-  def evaluate(guess_word)
-    @score = %w[B B B B B]
+  def evaluate(user_guess)
+    @results = %w[B B B B B]
     characters_of_the_day.each_index do |index|
-      @score[index] = Constants::YELLOW if characters_of_the_day.include? guess_word[index]
-      @score[index] = Constants::GREEN if guess_word[index] == characters_of_the_day[index]
+      @results[index] = Constants::YELLOW if characters_of_the_day.include? user_guess[index]
+      @results[index] = Constants::GREEN if user_guess[index] == characters_of_the_day[index]
     end
-    guess_word
+    user_guess
   end
 
   private
 
-  attr_accessor :attempt, :history
+  attr_accessor :attempt, :archive
   attr_reader :characters_of_the_day
 
-  def banner
+  def start_banner
     puts
     puts "Attempt #{attempt}"
   end
 
-  def guess
+  def user_input
+    word = evaluate(user_guess)
+    word.each {|character| print character.yellow << ' '}
+    puts
+  end
+
+  def user_guess
     word = gets.chomp.downcase
-    end_game_banner if word == 'x'
+    end_banner if word == 'x'
     puts
     if word.length != 5
       puts Constants::WORD_LENGTH
@@ -73,32 +76,32 @@ class Wordle
     end
   end
 
-  def add_to_history
-    @history << score
+  def archive_results
+    @archive << results
   end
 
-  def increment
+  def increment_attempt
     @attempt += 1
   end
 
-  def show_score(colors = score)
+  def show_results(colors = results)
     colormap = Colors.add_color(colors)
     colormap.each { |color| print color << '|' }
+    puts
   end
 
-  def show_history
-    history.each do |colors|
-      show_score(colors)
-      puts
+  def retrieve_archive
+    archive.each do |colors|
+      show_results(colors)
     end
   end
 
-  def end_game_banner
+  def end_banner
     puts
     puts "H i s t o r y:".yellow
-    show_history
+    retrieve_archive
     puts
-    puts score == Constants::ALL_GREEN ? 'Well done!' : 'Sorry, you did not get it this time!'
+    puts results == Constants::ALL_GREEN ? 'Well done!' : 'Sorry, you did not get it this time!'
     puts "The word you are looking for is: #{characters_of_the_day.join.green}"
     puts 'Do you want to play again? (y/n)'
     Wordle.play if gets.downcase.chomp == 'y'
--- tests/wordle_test.rb
+++ tests/wordle_test.rb
@@ -12,53 +12,50 @@ class Wordle_test < Minitest::Test
   def test_stdin_guess_is_wordw
     game = Wordle.new('words')
     game.evaluate($stdin.gets(2))
-    assert_equal %w[G G G G Y], game.score
+    assert_equal %w[G G G G Y], game.results
   end
 
   def test_stdin_guess_is_match
     game = Wordle.new('watch')
     game.evaluate($stdin.gets(4))
-    assert_equal %w[B G G G G], game.score
+    assert_equal %w[B G G G G], game.results
   end
 
   def test_without_stdin
     game = Wordle.new('words')
     game.evaluate('words')
-    assert_equal %w[G G G G G], game.score
+    assert_equal %w[G G G G G], game.results
   end
 
   def test_guess_is_words
     game = Wordle.new('words')
     game.evaluate('words')
-    assert_equal %w[G G G G G], game.score
+    assert_equal %w[G G G G G], game.results
   end
 
   def test_guess_is_watch
     game = Wordle.new('match')
     game.evaluate('watch')
-    assert_equal %w[B G G G G], game.score
+    assert_equal %w[B G G G G], game.results
   end
 
-  def test_guess_is_gross
-    game = Wordle.new('gloss')
+  def test_guess_is_gross game = Wordle.new('gloss')
     game.evaluate('gross')
-    assert_equal %w[G B G G G], game.score
+    assert_equal %w[G B G G G], game.results
   end
 
   def test_guess_is_abeam
     game = Wordle.new('abdul')
     game.evaluate('abeam')
-    assert_equal %w[G G B Y B], game.score
+    assert_equal %w[G G B Y B], game.results
   end
 
   def test_word_is_not_in_wordlist
-    game = Wordle.new
-    assert_equal false, game.valid_word?('xxxxx')
+    assert_equal false, Wordle.new.valid_word?('xxxxx')
   end
 
   def test_word_is_in_wordlist
-    game = Wordle.new
-    assert_equal true, game.valid_word?('sword')
+    assert_equal true, Wordle.new.valid_word?('sword')
   end
 
 end
