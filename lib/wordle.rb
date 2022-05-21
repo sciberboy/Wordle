25d2cd060e4f3736d087bed08f41ab836640b5d8
Stephan <sciberboy@gmail.com>
2022-05-21 10:42:35 +0200

    increase test coverage

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -2,74 +2,65 @@ require_relative './../config/config'
 
 class Wordle
 
-  attr_accessor  :score
-  attr_reader  :dictionary, :word_of_the_day
-
-  def play
-    until score == Constants::ALL_GREEN || attempt > Constants::ATTEMPT_LIMIT
-      start_banner
-      user_input
-      show_score
-      archive_score
-      increment_attempt
-    end
-    end_banner
-    play_again? ? Wordle.new.play : (puts Constants::BYE)
-    exit!
-  end
-
-
-  def evaluate(user_guess)
-    @score = %w[B B B B B]
-    characters_of_the_day.each_index do |index|
-      @score[index] = Constants::YELLOW if characters_of_the_day.include? user_guess[index]
-      @score[index] = Constants::GREEN if user_guess[index] == characters_of_the_day[index]
-    end
-    user_guess
-  end
-
-  def valid_word?(word)
-    dictionary.any? { |line| line.chomp == word }
-  end
-
-  def validate(word)
-    if word == 'x'
-      end_banner
-      exit!
-    elsif word.length != 5
-      puts Constants::WORD_LENGTH
-      play
-    elsif !valid_word?(word)
-      puts Constants::WORD_INVALID
-      play
-    else
-      word.chars
-    end
-  end
-
-  private
-
-  attr_accessor :attempt, :archive
+  attr_accessor :attempt, :archive, :score
   attr_reader :characters_of_the_day, :dictionary
+  attr_reader  :dictionary, :word_of_the_day
 
   def initialize(dictionary = File.readlines(Constants::WORDS_LIST))
     @dictionary = dictionary
     @word_of_the_day = dictionary.sample.chomp
-    @characters_of_the_day = word_of_the_day.downcase.chars
     @attempt = 1
     @score = []
     @archive = []
     puts "The word of the day is: #{word_of_the_day.green}" if $DEBUG
   end
 
+  def play
+    until score == Constants::ALL_GREEN || attempt > Constants::ATTEMPT_LIMIT
+      start_banner
+      word = gets.chomp.downcase
+      if word == 'x'
+        end_this_game
+      elsif word.length != 5
+        puts Constants::WORD_LENGTH
+      elsif !valid_word?(word)
+        puts Constants::WORD_INVALID
+      else
+        evaluate(word)
+        show_score
+        archive_score
+        increment_attempt
+      end
+    end
+    end_this_game
+  end
+
+  def end_this_game
+    end_banner
+    play_again? ? Wordle.new.play : (puts Constants::BYE)
+    exit!
+  end
+
+  def valid_word?(word)
+    dictionary.any? { |line| line.chomp == word }
+  end
+
+
+  def evaluate(user_guess, word = word_of_the_day)
+    guess = user_guess.downcase.chars
+    todays_characters = word.downcase.chars
+    @score = %w[B B B B B]
+    todays_characters.each_index do |index|
+      @score[index] = Constants::YELLOW if todays_characters.include? guess[index]
+      @score[index] = Constants::GREEN if guess[index] == todays_characters[index]
+    end
+    user_guess
+  end
+
+
   def start_banner
     puts
     puts "Attempt #{attempt}"
-  end
-
-  def user_input(guess = gets.chomp.downcase)
-    evaluate(validate(guess)).each {|character| print character.yellow << ' '}
-    puts
   end
 
   def archive_score
@@ -105,5 +96,4 @@ class Wordle
     puts 'Do you want to play again? (y/n)'
     true if gets.downcase.chomp == 'y'
   end
-
 end
--- tests/wordle_test.rb
+++ tests/wordle_test.rb
@@ -5,14 +5,14 @@ require 'wordle'
 class Wordle_test < Minitest::Test
 
   def test_guesss_is_words
-    game = Wordle.new(File.readlines('./tests/test_words.txt'))
-    game.evaluate('words')
+    game = Wordle.new
+    game.evaluate('words', 'words')
     assert_equal %w[G G G G G], game.score
   end
 
   def test_guess_is_watch
-    game = Wordle.new(File.readlines('./tests/test_match.txt'))
-    game.evaluate('watch')
+    game = Wordle.new
+    game.evaluate('watch', 'match')
     assert_equal %w[B G G G G], game.score
   end
 
